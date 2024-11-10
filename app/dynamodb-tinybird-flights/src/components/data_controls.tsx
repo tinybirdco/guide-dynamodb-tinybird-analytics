e822f7d50726c2bf39e78fcdb9c33884e5984133
sdairs <sdairs@users.noreply.github.com>
2024-11-10 00:15:33 +0000

    send recent

--- app/dynamodb-tinybird-flights/src/components/data_controls.tsx
+++ app/dynamodb-tinybird-flights/src/components/data_controls.tsx
@@ -8,7 +8,7 @@ export default function DataControls({ user, onDataSent }: { user: User, onDataS
         <div id="dataControls">
             <span className="align-middle">Fake data:</span>
             <div className="flex gap-2">
-                <SendFakeBookings amount={1} type="user" now={false} user={user} button_text="1 row from current user" callback={onDataSent} />
+                <SendFakeBookings amount={1} type="user" now={true} user={user} button_text="1 row from current user" callback={onDataSent} />
                 <SendFakeBookings amount={1} type="company" now={false} user={user} button_text="1 row from current company" callback={onDataSent} />
                 <SendFakeBookings amount={20} type="company" now={false} user={user} button_text="20 rows from current company" callback={onDataSent} />
                 <SendFakeBookings amount={20} type="random" now={false} user={user} button_text="20 rows from random users" callback={onDataSent} />
